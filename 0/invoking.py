2d35b3d2d9a6d809aa9f7f7ed5d10027e7fb6530
Mariana <57501545+wari99@users.noreply.github.com>
2025-11-02 19:28:27 -0300

    Delete 0/invoking.py

--- 0/invoking.py
+++
@@ -1,8 +0,0 @@
-import os
-from langchain_google_genai import ChatGoogleGenerativeAI
-
-os.environ["GOOGLE_API_KEY"] = "AIzaSyDifMetEsN05Zw9sC4PhSV3tflKAczBqo4"
-
-model = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite")
-
-print(model.invoke("Quais são as receitas mais famosas na America do Sul?"))
