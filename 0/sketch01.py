5cebe054b5b24cbc10e2b4a74e51d0ca321ac1ba
Mariana <57501545+wari99@users.noreply.github.com>
2025-11-11 17:58:11 -0300

    fix: simplified output

--- 0/sketch01.py
+++ 0/sketch01.py
@@ -8,14 +8,12 @@ from langchain.agents import create_agent
 os.environ["GOOGLE_API_KEY"] = ""
 model = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature= 0.5, max_output_tokens=500)
 
+def agent_result(question: str):
+    return agent.invoke({"messages": [{"role": "user", "content": question}]})
+
 agent = create_agent(
     model,
     system_prompt="Você é o ARCOS - RJ: agente especialista em informações do Portal de Dados Abertos do Rio de Janeiro. Sua missão é trazer informações relevantes, contextualizadas e rápidas sobre os dados presentes no Portal de Dados Abertos."
 )
 
-
-resultado = agent.invoke(
-    {"messages": [{"role": "user", "content": "Quem é voce"}]}
-)
-
-print(resultado)
+print(agent_result("Quem é você?"))
