5934679587d08dbaec1dafc53ad3746e04bf6f07
Mariana <57501545+wari99@users.noreply.github.com>
2025-11-20 21:09:31 -0300

    create v_sketch_04.py with memory

---
+++ 0/r_skect_4.py
@@ -0,0 +1,57 @@
+import os
+from typing import Any, TypedDict
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.tools import tool, ToolRuntime
+from langchain.agents import create_agent, AgentState
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.graph import StateGraph
+from dataclasses import dataclass
+
+os.environ["GOOGLE_API_KEY"] = ""
+
+@dataclass
+class Context:
+    user_id: str
+
+@dataclass
+class ResponseFormat:
+    summary: str
+
+model = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0.5,
+)
+
+agent = create_agent(
+    model=model,
+    context_schema=Context,
+    system_prompt="Você é o ARCOS - RJ..."
+)
+
+graph = StateGraph(AgentState)
+graph.add_node("inicio", agent)
+graph.set_entry_point("inicio")
+
+checkpointer = InMemorySaver()
+
+agent_memory = graph.compile(
+    checkpointer=checkpointer
+)
+
+print("Bem vindo ao ARCOS-RJ! Digite '/sair' para encerrar.\n")
+
+while True:
+    pergunta = input("Você: ").strip()
+
+    if pergunta.lower() == "/sair": 
+        print("ARCOS-RJ: Até logo!")
+        break
+
+    resultado = agent_memory.invoke(
+        {"messages": [{"role": "user", "content": pergunta}]},
+        config={"thread_id": "1"}
+    )
+
+    resposta = resultado["messages"][-1].content
+    print("ARCOS-RJ:", resposta)
